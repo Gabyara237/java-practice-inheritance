06ca199c84e050dfd816cfe8763ca76dd62e6230
Gabriela Araujo <gabyara237@gmail.com>
2025-03-13 01:14:07 -0700

    Add the DiceGame class: add constructor, add getters and setters, add toString method that displays the name of DiceGame inherited from the TablaTopGame class

---
+++ src/DiceGame.java
@@ -0,0 +1,30 @@
+public class DiceGame extends TableTopGame{
+    private int numDice = 0;
+    private int numDieSides = 6;
+
+    public DiceGame(int nd, int nds){
+        numDice= nd;
+        numDieSides = nds;
+    }
+
+    public int getNumDice() {
+        return numDice;
+    }
+
+    public int getNumDieSides() {
+        return numDieSides;
+    }
+
+    public void setNumDice(int numDice) {
+        this.numDice = numDice;
+    }
+
+    public void setNumDieSides(int numDieSides) {
+        this.numDieSides = numDieSides;
+    }
+
+    @Override
+    public String toString() {
+        return "DiceGame: " + super.getName();
+    }
+}
